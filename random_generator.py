6e122376ef7ca6ab20740edbe237d43e3b82158d
Dougc1 <72005771+Dougc1@users.noreply.github.com>
2021-07-10 20:18:43 +1000

    Create random_generator.py

---
+++ random_generator.py
@@ -0,0 +1,21 @@
+import random
+import math
+
+dict = {}
+
+for i in range(8,24):
+    dict[i] = 0
+math.floor(2.5)
+
+
+for i in range(100):
+    #print(math.floor(8*(1+2*random.random())))
+    dict[math.floor(8*(1+2*random.random()))] += 1
+
+matrix = []
+for i in range(5):
+    matrix.append([])
+
+print(matrix)
+
+print(dict)
