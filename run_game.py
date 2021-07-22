258b45b2f5309e59bc6f21c8574b5dd6c5d7fe66
Dougc1 <72005771+Dougc1@users.noreply.github.com>
2021-07-22 11:48:04 +1000

    Update run_game.py

--- run_game.py
+++ run_game.py
@@ -1,22 +1,21 @@
 from gameEngine import GameEngine, NPCRandomBot
-
-# import your bots here
 import importlib
 
+# List your bots here
 botsToRun = {
-    "bot_V1":2,
-    "template":2,
+    "examples.randomBidder":3,
+    "examples.randomAccuser":3,
+    "examples.randomSwapper":3,
     "NPC": 3
 }
 
 engine = GameEngine()
 
-# Warning: Timeouts are not enforced locally - so if you have an infinite loop, beware!
 for b in botsToRun:
-    if b=="NPC":
-        engine.registerBot(NPCRandomBot(),team="NPC")
-    else:
-        botClass = importlib.import_module(b)
-        for i in range(botsToRun[b]):
+    for i in range(botsToRun[b]):
+        if b=="NPC":
+            engine.registerBot(NPCRandomBot(),team="NPC")
+        else:
+            botClass = importlib.import_module(b)
             engine.registerBot(botClass.CompetitorInstance(),team=b)
 engine.runGame()
