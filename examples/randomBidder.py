5ca56df28b9efda5d3b3fdae3b4daf0fab181a8d
Dougc1 <72005771+Dougc1@users.noreply.github.com>
2021-08-31 14:47:54 +1000

    Update randomBidder.py

--- examples/randomBidder.py
+++ examples/randomBidder.py
@@ -7,16 +7,30 @@ class CompetitorInstance():
     def onGameStart(self, engine, gameParameters):
         # engine: an instance of the game engine with functions as outlined in the documentation.
         self.engine=engine
+        self.gameParameters = gameParameters
     
     def onAuctionStart(self, index, trueValue):
+        self.round = 0
         pass
 
     def onBidMade(self, whoMadeBid, howMuch):
         pass
 
     def onMyTurn(self,lastBid):
-        if self.engine.random.randint(0,100)<50:
-            self.engine.makeBid(lastBid+111)
+        
+        """ if self.round > :
+            return """
+            
+
+        if self.round == 3:
+            self.engine.makeBid(15000)
+            
+
+
+        """ if self.engine.random.randint(0,100)<50:
+            self.engine.makeBid(lastBid+8) """
+        
+        self.round += 1
         pass
 
     def onAuctionEnd(self):
